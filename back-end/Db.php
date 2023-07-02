d476c4818249434c998d6850ebe1dc4add5b4dc2
Stefania Tsvetkova <nia_flo@abv.bg>
2023-07-03 00:08:24 +0300

    Extracted db constrants

--- back-end/Db.php
+++ back-end/Db.php
@@ -1,16 +1,16 @@
 <?php
+    require_once "constants.php";
+
     class Db
     {
         private $connection;
 
         public function __construct()
         {
-            $dbhost = "localhost";
-            $dbName = "file_management_system";
-            $userName = "root";
-            $userPassword = "";
-
-            $this->connection = new PDO("mysql:host=$dbhost;dbname=$dbName", $userName, $userPassword,
+            $this->connection = new PDO(
+                "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME,
+                DB_USER,
+                DB_PASSWORD,
                 [
                     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
---
+++ back-end/constants.php
@@ -0,0 +1,9 @@
+<?php
+    // Change these constants if needed 
+    const DB_HOST = "localhost";
+    const DB_USER = "root";
+    const DB_PASSWORD = "";
+
+    // Don't change these constants:
+    const DB_NAME = "file_management_system";
+?>
